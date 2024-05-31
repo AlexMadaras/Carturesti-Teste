1ec2310f371b279d96bceb1e997904e929830bf4
AlexMadaras <163894151+AlexMadaras@users.noreply.github.com>
2024-05-31 18:49:41 +0300

    31.05

--- src/main/java/Pages/ProductsAndCart.java
+++ src/main/java/Pages/ProductsAndCart.java
@@ -18,10 +18,6 @@ public class ProductsAndCart {
         WebElement addProductToCart = driver.findElement(By.xpath("//*[@id=\"cartu-add-to-cart-btn-x\"]/a"));
         addProductToCart.click();
     }
-    public void addToWishlist(){
-        WebElement addProductToWishlist = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/md-card/md-card-content/div[1]/a"));
-        addProductToWishlist.click();
-    }
     public void viewCart(){
         WebElement viewCart = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div[2]/div/a"));
         viewCart.click();
@@ -29,5 +25,23 @@ public class ProductsAndCart {
     public void deleteFromCart(){
         WebElement deleteProductFromCart = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div[2]/div/div/div/div/table/tbody/tr/td[3]/a/i"));
         deleteProductFromCart.click();
+    }
+    public void addToWishlist(){
+        WebElement addProductToWishlist = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/md-card/md-card-content/div[1]/a"));
+        addProductToWishlist.click();
+    }
+    public void chooseWishList(){
+        WebElement chooseWishlist = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/md-card/md-card-content/div[1]/div/div[1]"));
+        chooseWishlist.click();
+    }
+    public void goToWishlist(){
+        WebElement clickWishlist = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div/div[2]/md-menu-bar[2]/md-menu-item[3]/ul/li[3]/a"));
+        clickWishlist.click();
+    }
+    public void removeFromWishlist(){
+        WebElement removeItemFromWishlist = driver.findElement(By.xpath("/html/body/div[4]/div[2]/div[2]/div[2]/div/md-card/md-card-content/md-list/md-card/md-card-actions/i[2]"));
+        removeItemFromWishlist.click();
+        WebElement confirmRemove = driver.findElement(By.xpath("/html/body/div[9]/md-dialog/md-dialog-actions/button[2]"));
+        confirmRemove.click();
     }
 }
--- src/test/java/TestePaC.java
+++ src/test/java/TestePaC.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.bidi.log.Log;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -8,30 +9,88 @@ import org.testng.Assert;
 import Pages.Homepage;
 import Pages.LogIn;
 import Pages.ProductsAndCart;
+
+import java.time.Duration;
+
 public class TestePaC extends BaseTest{
-    @org.testng.annotations.Test
-    public void logIn() throws InterruptedException {
+    @org.testng.annotations.Test // Adaugare produs in Cos
+    public void addInCart() throws InterruptedException{
         LogIn login = new LogIn(driver);
         ProductsAndCart productsAndCart = new ProductsAndCart(driver);
-        Thread.sleep(1000);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         login.clickLogIn();
-        Thread.sleep(1000);
         login.clickExistingUser();
-        Thread.sleep(1000);
         login.enterEmail();
         Thread.sleep(500);
         login.enterPassword();
-        Thread.sleep(500);
         login.acceptCookies();
         login.clickAuthenticate();
         Thread.sleep(1000);
         productsAndCart.clickFirstProduct();
-        Thread.sleep(5000);
+        Thread.sleep(2000);
         productsAndCart.addToCart();
         Thread.sleep(3000);
+    }
+    @org.testng.annotations.Test // Stergere produs din Cos
+    public void logIn() throws InterruptedException {
+        LogIn login = new LogIn(driver);
+        ProductsAndCart productsAndCart = new ProductsAndCart(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        login.clickLogIn();
+        login.clickExistingUser();
+        login.enterEmail();
+        Thread.sleep(500);
+        login.enterPassword();
+        login.acceptCookies();
+        login.clickAuthenticate();
+        Thread.sleep(1000);
+        productsAndCart.clickFirstProduct();
+        Thread.sleep(2000);
+        productsAndCart.addToCart();
+        Thread.sleep(2000);
         productsAndCart.viewCart();
         Thread.sleep(1000);
         productsAndCart.deleteFromCart();
+        Thread.sleep(3000);
+    }
+    @org.testng.annotations.Test // Adaugare produs in Wishlist
+    public void addWishlist() throws InterruptedException {
+        LogIn login = new LogIn(driver);
+        ProductsAndCart productsAndCart = new ProductsAndCart(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        login.clickLogIn();
+        login.clickExistingUser();
+        login.enterEmail();
+        Thread.sleep(500);
+        login.enterPassword();
+        login.acceptCookies();
+        login.clickAuthenticate();
+        Thread.sleep(1000);
+        productsAndCart.clickFirstProduct();
+        productsAndCart.addToWishlist();
+        Thread.sleep(500);
+        productsAndCart.chooseWishList();
+        Thread.sleep(1000);
+        login.accountDropdown();
+        productsAndCart.goToWishlist();
+        Thread.sleep(3000);
+    }
+    @org.testng.annotations.Test // Eliminare produs din Wishlist
+    public void removeFromWishlist() throws InterruptedException{
+        LogIn login = new LogIn(driver);
+        ProductsAndCart productsAndCart = new ProductsAndCart(driver);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        login.clickLogIn();
+        login.clickExistingUser();
+        login.enterEmail();
+        Thread.sleep(500);
+        login.enterPassword();
+        login.acceptCookies();
+        login.clickAuthenticate();
+        Thread.sleep(1000);
+        login.accountDropdown();
+        productsAndCart.goToWishlist();
+        productsAndCart.removeFromWishlist();
         Thread.sleep(3000);
     }
 }
